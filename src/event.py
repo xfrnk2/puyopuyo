dc7cb9127c05626f0ec6a2269b20c939e353358c
xfrnk2 <xfrnk2@gmail.com>
2021-02-23 20:51:15 +0900

    키 조작을 반영하는 EventManager class와 조작별 Event class 작성

---
+++ src/event.py
@@ -0,0 +1,55 @@
+from pynput.keyboard import Key, Listener, KeyCode
+from abc import ABC
+
+
+class Event(ABC):
+    pass
+
+class GameExitEvent(Event):
+    pass
+
+class MoveLeftEvent(Event):
+    pass
+
+class MoveRightEvent(Event):
+    pass
+
+class MoveDownEvent(Event):
+    pass
+
+class LeftSpinEvent(Event):
+    pass
+
+class RightSpinEvent(Event):
+    pass
+
+class VoidEvent(Event):
+    pass
+
+class EventManager:
+
+    def __init__(self):
+        self.__listener = Listener(on_release=__class__.on_release)
+        self.__listener.start()
+    @staticmethod
+    def on_release(key):
+        __class__.__current_key = key
+    @staticmethod
+    def get_event() -> Event:
+        handlers = {
+            Key.left: MoveLeftEvent,
+            Key.right: MoveRightEvent,
+            Key.down: MoveDownEvent,
+            Key.esc: GameExitEvent,
+            KeyCode(char='z'): LeftSpinEvent,
+            KeyCode(char='x'): RightSpinEvent
+        }
+
+        try:
+            current_key = __class__.__current_key
+            __class__.__current_key = None
+            return handlers[current_key]()
+        except KeyError:
+            return VoidEvent()
+
+    __current_key = None
