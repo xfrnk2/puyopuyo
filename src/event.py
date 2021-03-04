5106893c2d0b0031df9c545c0723716e34e5bb40
xfrnk2 <xfrnk2@gmail.com>
2021-03-04 22:49:06 +0900

    Code Reformatting

--- src/event.py
+++ src/event.py
@@ -1,39 +1,50 @@
-from pynput.keyboard import Key, Listener, KeyCode
 from abc import ABC
+
+from pynput.keyboard import Key, KeyCode, Listener
 
 
 class Event(ABC):
     pass
 
+
 class GameExitEvent(Event):
     pass
+
 
 class MoveLeftEvent(Event):
     pass
 
+
 class MoveRightEvent(Event):
     pass
+
 
 class MoveDownEvent(Event):
     pass
 
+
 class LeftSpinEvent(Event):
     pass
+
 
 class RightSpinEvent(Event):
     pass
 
+
 class VoidEvent(Event):
     pass
+
 
 class EventManager:
 
     def __init__(self):
         self.__listener = Listener(on_release=__class__.on_release)
         self.__listener.start()
+
     @staticmethod
     def on_release(key):
         __class__.__current_key = key
+
     @staticmethod
     def get_event() -> Event:
         handlers = {
