0801d36a3c01288f07768a014acfc0ef134bfd92
xfrnk2 <xfrnk2@gmail.com>
2021-03-04 18:36:09 +0900

    불필요한 파일 제거

--- src/puyo.py
+++
@@ -1,7 +0,0 @@
-class Puyo:
-    def __init__(self):
-        self.__x = 1
-        self.__y = 1
-
-    def get_location(self):
-        return self.__x, self.__y
--- tests/puyo_test.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('C:/Users/rad87/Documents/programming/puyopuyo')
-from src.puyo import Puyo
-
-def test_location():
-    puyo = Puyo()
-    assert puyo.get_location() == (1, 1)
