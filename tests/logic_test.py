4a0bb5a1cb492be23d7f0f183289b91289a64ab7
xfrnk2 <xfrnk2@gmail.com>
2021-03-14 14:08:56 +0900

    logic_test.py : PEP8을 따르도록 수정

--- tests/logic_test.py
+++ tests/logic_test.py
@@ -1,20 +1,23 @@
 import sys
+from dataclasses import dataclass
+
+from src.logic import Logic
+
 sys.path.append('C:/Users/rad87/Documents/programming/puyopuyo')
 
-from dataclasses import dataclass
-from src.logic import Logic
 
 @dataclass
 class ValidCoordinatesCase:
     position: tuple
     expected: bool
 
+
 def test_valid_coordinates():
-    cases=(ValidCoordinatesCase(position=(12,20), expected=False),
-           ValidCoordinatesCase(position=(-1,6), expected=False),
-           ValidCoordinatesCase(position=(0, 3), expected=True),
-           ValidCoordinatesCase(position=(11, 19), expected=True)
-           )
+    cases = (ValidCoordinatesCase(position=(12, 20), expected=False),
+             ValidCoordinatesCase(position=(-1, 6), expected=False),
+             ValidCoordinatesCase(position=(0, 3), expected=True),
+             ValidCoordinatesCase(position=(11, 19), expected=True)
+             )
     logic = Logic()
 
     for case in cases:
