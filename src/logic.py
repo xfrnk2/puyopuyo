42ddacc6bf478fea1d9f49e37dcc2d33c8d69d48
xfrnk2 <xfrnk2@gmail.com>
2021-03-14 14:08:27 +0900

    유효좌표인지 확인하는 함수 작성

---
+++ src/logic.py
@@ -0,0 +1,37 @@
+import copy
+
+WIDTH = 12
+HEIGHT = 20
+
+
+class Logic:
+    def __init__(self, width=WIDTH, height=HEIGHT):
+        self.__width = width
+        self.__height = height
+        self.__original_logic = [[None for _ in range(width)] for _ in range(height)]
+        self.__logic = None
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    def update(self, puyo_objects):
+        self.__logic = copy.deepcopy(self.__original_logic)
+        for puyo in puyo_objects:
+            x, y = puyo.position
+            self.__logic[self.__height - y - 1][x] = puyo
+
+    def already_exist(self, puyo, position):
+        try:
+            x, y = puyo.position
+            return self.__logic[self.__height - y - 1][x] or self.__logic[self.__height - y - 1][x] == puyo
+        except IndexError:
+            pass
+
+    def valid_coordinates(self, position):
+        x, y = position
+        return 0 <= y < self.__height and 0 <= x < self.__width
