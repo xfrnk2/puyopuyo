875097b89a33ccabca1fb195de5e99c13ff69f06
xfrnk2 <xfrnk2@gmail.com>
2021-03-16 13:00:22 +0900

    낙하 가능여부에 대한 테스트코드 작성 이벤트를 좌표로반영하는 테스트코드 작성

--- tests/puyo_test.py
+++ tests/puyo_test.py
@@ -1,31 +1,47 @@
 from dataclasses import dataclass
-
+from src.logic import Logic
 from src.event import Event, MoveDownEvent, MoveLeftEvent, MoveRightEvent
 from src.puyo import Puyo
 
+LOGIC = Logic()
+
 
 @dataclass
-class PuyoUpdateCase:
-    time: float
+class EventReflectCase:
     event: Event
-    prev_position = tuple
+    prev_position: tuple
     expected: tuple
 
 
-class PuyoMock(Puyo):
-    def __init__(self, position):
-        x, y = position
-        self._x = x
-        self._y = y
+@dataclass
+class FallingCase:
+    time: float
+    expected: bool
 
 
-def test_puyo_update():
-    cases = (PuyoUpdateCase(time=0.01, event=MoveLeftEvent, prev_position=(6, 20), expected=(5, 20)),
-             PuyoUpdateCase(time=1.1, event=MoveRightEvent, prev_position=(6, 20), expected=(7, 19)),
-             PuyoUpdateCase(time=1.1, event=MoveDownEvent, prev_position=(2, 15), expected=(2, 13)),
-             PuyoUpdateCase(time=0.1, event=MoveDownEvent, prev_position=(2, 15), expected=(2, 14)),
-             )
+def test_event_reflect():
+    cases = (
+        EventReflectCase(event=MoveLeftEvent, prev_position=(6, 5), expected=(5, 5)),
+        EventReflectCase(event=MoveRightEvent, prev_position=(6, 6), expected=(7, 6)),
+        EventReflectCase(event=MoveDownEvent, prev_position=(2, 15), expected=(2, 16))
+            )
+
+
     for case in cases:
-        puyo = PuyoMock(case.prev_position)
-        puyo.update(case.time, case.event)
-        assert puyo.position == case.expected
+        event, prev_position, expected = case.event, case.prev_position, case.expected
+        puyo = Puyo(LOGIC)
+
+        puyo.position = prev_position
+        result = puyo.reflect_event(event())
+        assert result == expected
+
+def test_falling():
+    cases = (
+        FallingCase(time=0.1, expected=True),
+        FallingCase(time=0, expected=False)
+            )
+
+    for case in cases:
+        time, expected = case.time, case.expected
+        puyo = Puyo(LOGIC)
+        assert puyo.falling(time) == expected
