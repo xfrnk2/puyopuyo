89461a1405d35f14fe4686a3bc83943055f951fd
xfrnk2 <xfrnk2@gmail.com>
2021-03-17 22:55:59 +0900

    puyo_test.py : 인스턴스를 담는 변수를 전역 -> 각 개별 테스트 내부에서 생성하도록 변경

--- tests/puyo_test.py
+++ tests/puyo_test.py
@@ -3,8 +3,6 @@ from src.logic import Logic
 from src.event import Event, MoveDownEvent, MoveLeftEvent, MoveRightEvent
 from src.puyo import Puyo
 
-LOGIC = Logic()
-
 
 @dataclass
 class EventReflectCase:
@@ -20,6 +18,7 @@ class FallingCase:
 
 
 def test_event_reflect():
+    logic = Logic()
     cases = (
         EventReflectCase(event=MoveLeftEvent, prev_position=(6, 5), expected=(5, 5)),
         EventReflectCase(event=MoveRightEvent, prev_position=(6, 6), expected=(7, 6)),
@@ -29,13 +28,14 @@ def test_event_reflect():
 
     for case in cases:
         event, prev_position, expected = case.event, case.prev_position, case.expected
-        puyo = Puyo(LOGIC)
+        puyo = Puyo(logic)
 
         puyo.position = prev_position
         result = puyo.reflect_event(event())
         assert result == expected
 
 def test_falling():
+    logic = Logic()
     cases = (
         FallingCase(time=0.1, expected=True),
         FallingCase(time=0, expected=False)
@@ -43,5 +43,5 @@ def test_falling():
 
     for case in cases:
         time, expected = case.time, case.expected
-        puyo = Puyo(LOGIC)
+        puyo = Puyo(logic)
         assert puyo.falling(time) == expected
