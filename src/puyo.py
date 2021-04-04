24bb49e581ff56b6e7f5f001e00ebddd0fd6cf19
xfrnk2 <xfrnk2@gmail.com>
2021-04-05 01:30:21 +0900

    Puyo 클래스의 인스턴스 객체 기능 축소

--- src/puyo.py
+++ src/puyo.py
@@ -1,9 +1,10 @@
 from uuid import uuid4
 import copy
-from event import (LeftSpinEvent, MoveDownEvent, MoveLeftEvent,
-                       MoveRightEvent, RightSpinEvent, VoidEvent)
+from event import (Event, LeftSpinEvent, MoveDownEvent, MoveLeftEvent,
+                   MoveRightEvent, RightSpinEvent, VoidEvent, SpinEvent, MoveEvent)
 from logic import Logic
 from timer import Timer
+
 
 WIDTH = 12
 HEIGHT = 20
@@ -20,6 +21,13 @@ class Puyo:
 
         self.__logic: Logic = logic
 
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     @property
     def id(self):
@@ -50,47 +58,8 @@ class Puyo:
         x, y = coordinates
         self.__x, self.__y = x, y
 
-    def reflect_event(self, event):
-        if isinstance(event, VoidEvent):
-            return
-        x, y = self.__x, self.__y
+    def falling(self):
+        self.__y -= 1
 
-        if isinstance(event, MoveLeftEvent):
-            x -= 1
-        elif isinstance(event, MoveRightEvent):
-            x += 1
-        elif isinstance(event, MoveDownEvent):
-            y -= 1
-        if isinstance(event, LeftSpinEvent):
-            x -= 1
-            y -= 1
-        elif isinstance(event, RightSpinEvent):
-            x += 1
-            y -= 1
-
-        return x, y
-
-
-    def update(self, time, event):
-        if not self.valid:
-            return
-
-        new_position = self.reflect_event(event)
-        is_updatable = 'self.logic.valid_coordinates(new_position) and ' \
-                       'not self.logic.already_exist(self, new_position)'
-
-        if new_position and eval(is_updatable):
-            self.position = new_position
-
-        y = self.__y
-        y -= self.__speed * Timer.get_elapsed()
-        y = max(y, 0)
-
-        new_position = (self.__x, int(y))
-        if not eval(is_updatable):
-            self.valid = False
-            return
-
-        self.__y = y
-        if int(self.__y) == 0:
-            self.valid = False
+    def is_updatable(self, new_position):
+        return self.logic.valid_coordinates(new_position) and not self.logic.already_exist(self, new_position)
